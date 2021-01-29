2b83197cb7109ace7174e3120f8d41d43ed7cc54
= <bperrett@fusionbox.com>
2021-01-28 22:39:55 -0700

    Use asyncio for communicating with twitch

--- main.py
+++ main.py
@@ -1,34 +1,66 @@
+import asyncio
 from twitch import TwitchClient
 from huecontroller import Lights
 import select
 
-tc = TwitchClient()
+
 li = Lights(transitiontime=.2)
 
-def handle_message(message):
+async def handle_message(message, tc):
     print(message)
     [*p1, m] = message.split('#import_antigrvty :')
     n = m.split()[1] if len(m.split()) > 1 else ''
+    try:
+        n = int(n)
+    except:
+        n = -1
 
-    if m.startswith('!off'):
+    if message.startswith('PING'):
+        print('found ping message')
+        tc.writer.write('PONG'.encode('utf-8'))
+    elif m.startswith('!commands'):
+        m = [
+            'Light commands: ',
+            '!hue <n>, !bri <n>, !sat <n>, !on, !off'
+        ]
+        m = ' '.join(m)
+        tc.send(m)
+    elif m.startswith('!off'):
         li.all_off()
+        tc.send('Turning off the lights.')
     elif m.startswith('!on'):
         li.all_on()
+        tc.send('Turning on the lights.')
     elif m.startswith('!hue'):
         li.set_hue(n)
+        if 0 <= n <= 100:
+            tc.send(f'Setting hue to {n}.')
+        else:
+            tc.send(f'Argument must be between 0 and 100.')
     elif m.startswith('!bri'):
         li.set_brightness(n)
+        if 0 <= n <= 100:
+            tc.send(f'Setting brightness to {n}.')
+        else:
+            tc.send(f'Argument must be between 0 and 100.')
     elif m.startswith('!sat'):
         li.set_saturation(n)
+        if 0 <= n <= 100:
+            tc.send(f'Setting saturation to {n}.')
+        else:
+            tc.send(f'Argument must be between 0 and 100.')
 
 
-def main():
-    tc.socket.setblocking(0)
+async def main(tc):
+    await tc.connect()
     while True:
-        ready = select.select([tc.socket], [], [], 5)
-        if ready[0]:
-            r = tc.socket.recv(2048).decode('utf-8')
-            handle_message(r)
+        if hasattr(tc, 'reader'):
+            r = await tc.read()
+            await handle_message(r, tc)
+        else:
+            await asyncio.sleep(1)
 
 if __name__ == '__main__':
-    main()
+    tc = TwitchClient()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(tc))
--- twitch.py
+++ twitch.py
@@ -13,19 +13,41 @@ TOKEN = os.getenv('TOKEN')
 
 
 class TwitchClient():
-    def __init__(self):
-        self.socket = self.connect()
+    async def connect(self):
+        self.reader, self.writer = await asyncio.open_connection(host=HOST, port=PORT)
+        self.writer.write(f"PASS {TOKEN}\n".encode('utf-8'))
+        self.writer.write(f"NICK {NICKNAME}\n".encode('utf-8'))
+        self.writer.write(f"JOIN {CHANNEL}\n".encode('utf-8'))
 
-    def connect(self):
-        sock = socket.socket()
-        sock.connect((HOST, PORT))
-        sock.send(f"PASS {TOKEN}\n".encode('utf-8'))
-        sock.send(f"NICK {NICKNAME}\n".encode('utf-8'))
-        sock.send(f"JOIN {CHANNEL}\n".encode('utf-8'))
-        return sock
+    async def read(self, buffer=2048):
+        r = await self.reader.read(buffer)
+        r = r.strip().decode('utf-8')
+        return r
+
+    def send(self, message):
+        message = f'PRIVMSG {CHANNEL} :{message}\n'.encode('utf-8')
+        self.writer.write(message)
+
+    async def read_forever(self):
+        await self.connect()
+        while True:
+            if hasattr(tc, 'reader'):
+                r = await tc.read()
+                yield r
+            else:
+                await asyncio.sleep(1)
+
+async def main():
+    tc = TwitchClient()
+    await tc.connect()
+    while True:
+        if hasattr(tc, 'reader'):
+            r = await tc.read()
+            print(r)
+        else:
+            await asyncio.sleep(1)
+
 
 if __name__ == '__main__':
-    tc = TwitchClient()
-    while True:
-        resp = tc.socket.recv(2048).decode('utf-8')
-        print(resp)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
